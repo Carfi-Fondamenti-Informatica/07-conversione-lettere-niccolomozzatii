8968f27ce49e3458206d76d98beccc156ef2eec2
niccolomozzatii <118431871+niccolomozzatii@users.noreply.github.com>
2022-12-05 15:11:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+   char a=0;
+    cin >> a;
+    if(lettera(a)==true) {
+        cout << a << endl;
+    } else if(lettera(a)==false) {
+        cout << "errore" << endl;
+    }
   return 0;
 }
